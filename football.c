f4705637f1bad7f1c1edf592e31a088d0485bfc4
Sean Hammell <sean.m.hammell@gmail.com>
2023-10-12 10:06:43 -0500

    Finalize solution to NFL Score problem.

--- football.c
+++ football.c
@@ -1,56 +1,80 @@
 #include <stdio.h>
 
-#define TD_2PT  8
-#define TD_FG   7
-
-enum scoring_plays_idx {
-    TD_2PT_IDX,
-    TD_FG_IDX,
-    TOTAL_SCORING_PLAYS,
+/**
+ * types of scoring plays
+ */
+enum play_types {
+        TD_2PT,
+        TD_FG,
+        TD,
+        FG,
+        SAFETY,
+        N_PLAY_TYPES,
 };
 
-void print_scoring_plays(int *scoring_plays)
+/**
+ * value of each play type
+ */
+const int play_values[N_PLAY_TYPES] = {8, 7, 6, 3, 2};
+
+/**
+ * print_plays: print the combination of scoring plays that results in the final
+ * score
+ */
+void print_plays(int *plays)
 {
-    printf("%d TD + 2pt, ", scoring_plays[TD_2PT_IDX]);
-    printf("%d TD + FG\n", scoring_plays[TD_FG_IDX]);
+        printf("%d TD + 2pt, ", plays[TD_2PT]);
+        printf("%d TD + FG, ", plays[TD_FG]);
+        printf("%d TD, ", plays[TD]);
+        printf("%d 3pt FG, ", plays[FG]);
+        printf("%d Safety\n", plays[SAFETY]);
 }
 
-void n_touchdown_fg(int score, int *scoring_plays) {
-    for (int i = 0; i <= score / TD_FG; ++i) {
-        scoring_plays[TD_FG_IDX] = i;
-        if (score - (TD_FG * i) == 0) {
-            print_scoring_plays(scoring_plays);
+/**
+ * scoring_plays: recursively determine the possible combinations of scoring
+ * plays that equal the provided NFL score.
+ */
+void scoring_combinations(int score, int *plays, int play_type)
+{
+        /**
+         * once each play type has been checked, print the combination if it
+         * matches the NFL score
+         */
+        if (play_type == N_PLAY_TYPES) {
+                if (score == 0) {
+                        print_plays(plays);
+                }
+
+                return;
         }
-    }
-}
 
-void n_touchdown_2pt(int score, int *scoring_plays)
-{
-    for (int i = 0; i <= score / TD_2PT; ++i) {
-        scoring_plays[TD_2PT_IDX] = i;
-        n_touchdown_fg(score - (TD_2PT * i), scoring_plays);
-    }
-}
-
-void find_possible_scoring_combos(int score)
-{
-    printf("\nPossible Combinations of Scoring Plays\n");
-    int scoring_plays[TOTAL_SCORING_PLAYS] = {0};
-    n_touchdown_2pt(score, scoring_plays);
-    printf("\n");
+        /**
+         * loop over possible combinations of the current play type and
+         * recursively check the remaining play types
+         */
+        int max_plays = score / play_values[play_type];
+        for (int i = 0; i <= max_plays; ++i) {
+                plays[play_type] = i;
+                scoring_combinations(score - (play_values[play_type] * i),
+                                     plays,
+                                     play_type + 1);
+        }
 }
 
 int main()
 {
-    int score = 0;
-    while (1) {
-        printf("\nEnter 0 or 1 to STOP");
-        printf("\nEnter the NFL score: ");
-        scanf("%d", &score);
-        if (score < 2) {
-            return 0;
+        int score = 0;
+        int plays[N_PLAY_TYPES] = {0};
+        while (1) {
+                printf("\nEnter 0 or 1 to STOP");
+                printf("\nEnter the NFL score: ");
+                scanf("%d", &score);
+                if (score < 2) {
+                        return 0;
+                }
+                printf("\nPossible Combinations of Scoring Plays\n");
+                scoring_combinations(score, plays, TD_2PT);
         }
-        find_possible_scoring_combos(score);
-    }
-    return 0;
+
+        return 0;
 }
